0220a400658c096831794bb1f30cd286168829fa
ncip48 <mbahcip00@gmail.com>
2023-10-02 09:33:44 +0700

    export init

---
+++ app/Http/Controllers/ExportController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+class ExportController extends Controller
+{
+    private $token;
+
+    private function loginApi()
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.kampusmerdeka.kemdikbud.go.id/user/auth/login/mbkm',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{
+    "email": "herlycp@students.amikom.ac.id",
+    "password": "Mbahcip123"
+    }',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+            ),
+            CURLOPT_SSL_VERIFYPEER => false,
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        //change to stdClass
+        $response = json_decode($response);
+
+        $this->token = $response->data->token;
+        return;
+    }
+
+    private function fetchApi($method, $endpoint)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.kampusmerdeka.kemdikbud.go.id/' . $endpoint,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json',
+                'Authorization: Bearer ' . $this->token
+            ),
+            CURLOPT_SSL_VERIFYPEER => false,
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        //change to stdClass
+        $response = json_decode($response);
+
+        // return $response->data;
+
+        return $response->data;
+    }
+
+    private function getIdMagang()
+    {
+        $response = $this->fetchApi('GET', 'mbkm/mahasiswa/activities');
+
+        return $response[0]->id;
+    }
+
+    public function index(Request $request)
+    {
+        $minggu = $request->minggu;
+        $token = $request->token;
+        // $this->loginApi();
+        $this->token = $token ?? "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYTgxM2RhZDctYzYwYi00Mzc2LWJhNGQtZTYwNWVkNGQ4ZGY0IiwicnQiOmZhbHNlLCJleHAiOjE2OTYyMTQxNjUsImlhdCI6MTY5NjIxMjM2NSwiaXNzIjoiV2FydGVrLUlEIiwibmFtZSI6Ikhlcmx5IENoYWh5YSBQdXRyYSIsInJvbGVzIjpbIm1haGFzaXN3YSJdLCJwdF9jb2RlIjoiMDUxMDI0IiwiaGFzX2FkbWluX3JvbGUiOmZhbHNlLCJtaXRyYV9pZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCIsImVtYWlsIjoiIiwic2Vrb2xhaF9ucHNuIjoiIn0.xlJxny-bx7F5T8vm33CtCWv-ur8aghFQcnxs5i3QY9nH5Et0ZgeddzJrJXMkLA5YWSp4zXBqUD7px26HyA5uUw";
+
+        $id_activity = $this->getIdMagang();
+
+        $reports = $this->fetchApi('GET', 'magang/report/allweeks/' . $id_activity);
+
+        $location = $this->fetchApi('GET', 'magang/report/summary/' . $id_activity);
+        $location = $location->activity->brand_name;
+
+        $profile = $this->fetchApi('GET', 'mbkm/mahasiswa/profile');
+
+        $newReports = [];
+        setlocale(LC_ALL, 'IND');
+
+        foreach ($reports as $report) {
+            foreach ($report->daily_report as $d) {
+                $d->report_date = Carbon::parse($d->report_date)->formatLocalized('%A, %d %B %Y');
+                $newReports[] = [
+                    'report' => $d->report,
+                    'minggu' => $report->counter,
+                    'tanggal' => $d->report_date,
+                ];
+            }
+        }
+        //sort by minggu
+        usort($newReports, function ($a, $b) {
+            return $a['minggu'] <=> $b['minggu'];
+        });
+
+        $reports = json_encode($newReports);
+        $reports = json_decode($reports);
+
+
+
+        //if minggu is set
+        if ($minggu) {
+            $reports = array_filter($reports, function ($report) use ($minggu) {
+                return $report->minggu == $minggu;
+            });
+        }
+
+        //render dompdf
+        $pdf = PDF::loadView('report', compact('reports', 'location', 'minggu', 'profile'));
+
+        return $pdf->stream();
+
+        // return view('report', compact('reports', 'location', 'minggu', 'profile'));
+    }
+}
---
+++ resources/views/report.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Logbook</title>
+    <style>
+        * {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
+
+        th,
+        td {
+            padding: 5px;
+            text-align: left;
+        }
+
+        th {
+            font-weight: bold;
+            text-align: center;
+        }
+
+        h3 {
+            text-align: center;
+            margin: 0px;
+
+        }
+
+        .identity {
+            margin-bottom: 20px;
+        }
+
+        .identity tr td {
+            border: none !important;
+        }
+    </style>
+</head>
+
+<body>
+    <h3>LOGBOOK KEGIATAN</h3>
+    <h3>PRAKTEK KERJA LAPANGAN TAHUN AJAR 2023/2024</h3>
+    <h3>PROGRAM STUDI D3 TEKNIK INFORMATIKA</h3>
+    <h3 style="margin-bottom: 20px">MAGANG MBKM</h3>
+
+    <table class="identity" style="border: none">
+        <tr>
+            <td>Identitas</td>
+            <td>:</td>
+            <td>{{ $profile->nim }} {{ $profile->name }}</td>
+        </tr>
+        <tr>
+            <td>Nama Perusahaan</td>
+            <td>:</td>
+            <td>{{ $location }}</td>
+        </tr>
+        <tr>
+            <td>Minggu Ke</td>
+            <td>:</td>
+            <td>{{ $minggu }}</td>
+        </tr>
+    </table>
+
+    <table>
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Tanggal</th>
+                <th>Uraian Kegiatan</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($reports as $log)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $log->tanggal }}</td>
+                    <td>{{ $log->report }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+
+</html>
