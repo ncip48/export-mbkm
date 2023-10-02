fdfd3977a051129587d706104239b6ebdc84d46e
ncip48 <mbahcip00@gmail.com>
2023-10-02 12:54:27 +0700

    required

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,11 +50,11 @@
         @csrf
         <div class="form-group">
             <label for="email">*Email:</label>
-            <input type="email" id="email" name="email" required>
+            <input type="email" id="email" name="email">
         </div>
         <div class="form-group">
             <label for="password">*Password:</label>
-            <input type="password" id="password" name="password" required>
+            <input type="password" id="password" name="password">
         </div>
         <div class="form-group">
             <label for="nim">Token:</label>
